ddf4aedbcf46831356727ca82b9d5334a8937d1a
cianami <arina.dybal00@gmail.com>
2023-12-06 11:32:15 +0300

    Delete button added to the product card

--- app/src/main/java/com/example/caloriecalculator/ProductCard.kt
+++ app/src/main/java/com/example/caloriecalculator/ProductCard.kt
@@ -1,17 +1,19 @@
 package com.example.caloriecalculator
 
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material.icons.outlined.Kitchen
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Card(name:String, weight:Int, caloree:Int) {
@@ -24,15 +26,38 @@ fun Card(name:String, weight:Int, caloree:Int) {
             .offset(y = 30.dp)
             .padding(bottom = 10.dp)
     ) {
-        Text(
-            text = name,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier
-                .padding(16.dp)
-                .offset(y=-5.dp),
-            textAlign = TextAlign.Center,
-            color = Color(0xff2B4859)
-        )
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Column(
+                horizontalAlignment = Alignment.Start
+            ){
+                Text(
+                    text = name,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .offset(y = -5.dp),
+                    textAlign = TextAlign.Center,
+                    color = Color(0xff2B4859)
+                )
+            }
+            Column(
+                horizontalAlignment = Alignment.End,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                IconButton(
+                    onClick = { },
+                ) {
+                    Icon(
+                        Icons.Outlined.Close,
+                        contentDescription = "Localized description",
+                        modifier = Modifier.size(20.dp),
+                        tint = Color(0xff473366)
+                    )
+                }
+            }
+        }
         Text(
             text = "$weight г. - $caloree ккал",
             modifier = Modifier
